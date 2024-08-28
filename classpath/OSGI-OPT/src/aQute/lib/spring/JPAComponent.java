b306100eeb401c19cadc5f0872817ee9723c399b
Erfan Arvan <ea442@njit.edu>
2024-08-27 21:20:25 -0400

    firstWPI

---
+++ classpath/OSGI-OPT/src/aQute/lib/spring/JPAComponent.java
@@ -0,0 +1,25 @@
+package aQute.lib.spring;
+
+import java.util.*;
+
+import aQute.bnd.annotation.plugin.*;
+import aQute.bnd.osgi.*;
+
+/**
+ * This component is called when we find a resource in the META-INF/*.xml
+ * pattern. We parse the resource and and the imports to the builder. Parsing is
+ * done with XSLT (first time I see the use of having XML for the Spring
+ * configuration files!).
+ * 
+ * @author aqute
+ */
+@BndPlugin(name="jpa")
+public class JPAComponent extends XMLTypeProcessor {
+
+	@Override
+	protected List<XMLType> getTypes(Analyzer analyzer) throws Exception {
+		List<XMLType> types = new ArrayList<XMLType>();
+		process(types, "jpa.xsl", "META-INF", "persistence.xml");
+		return types;
+	}
+}
